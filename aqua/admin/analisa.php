12d234fb7c882fc5a10df1fecffea1082fc7a7b0
Nawcodes <nawcodes@gmail.com>
2021-12-25 13:08:37 +0700

    completing analisa & hasil

--- aqua/admin/analisa.php
+++ aqua/admin/analisa.php
@@ -21,9 +21,9 @@
             		<div class="panel-body">
             			
             			<div class="form-group">
-            				<label class="col-sm-3 control-label text-right">C1. Nilai IPK:</label>
+            				<label class="col-sm-3 control-label text-right">C1. Nilai Akhir:</label>
             				<div class="col-sm-8">
-            					<select name='bobot_nilai_ipk' class="required select">
+            					<select name='bobot_nilai_akhir' class="required select">
             						<option value="1">Sangat Rendah</option>
             						<option value="2">Rendah</option>
             						<option value="3">Cukup</option>
@@ -34,9 +34,9 @@
             			</div>
 
             			<div class="form-group">
-            				<label class="col-sm-3 control-label text-right">C2. Penghasilan Ortu:</label>
+            				<label class="col-sm-3 control-label text-right">C2. Jumlah Sertifikat:</label>
             				<div class="col-sm-8">
-            					<select name='bobot_penghasilan_ortu' class="required select">
+            					<select name='bobot_sertifikat' class="required select">
             						<option value="1">Sangat Rendah</option>
             						<option value="2">Rendah</option>
             						<option value="3">Cukup</option>
@@ -47,9 +47,9 @@
             			</div>
 
             			<div class="form-group">
-            				<label class="col-sm-3 control-label text-right">C3. Semester:</label>
+            				<label class="col-sm-3 control-label text-right">C3. Nilai Sikap:</label>
             				<div class="col-sm-8">
-            					<select name='bobot_semester' class="required select">
+            					<select name='bobot_nilai_sikap' class="required select">
             						<option value="1">Sangat Rendah</option>
             						<option value="2">Rrendah</option>
             						<option value="3">Cukup</option>
@@ -59,31 +59,6 @@
             				</div>
             			</div>
 
-            			<div class="form-group">
-            				<label class="col-sm-3 control-label text-right">C4. Jumlah Tanggungan:</label>
-            				<div class="col-sm-8">
-            					<select name='bobot_tanggungan' class="required select">
-            						<option value="1">Sangat Rendah</option>
-            						<option value="2">Rrendah</option>
-            						<option value="3">Cukup</option>
-            						<option value="4">Tinggi</option>
-            						<option value="5">Sangat Tinggi</option>
-            					</select>
-            				</div>
-            			</div>
-
-            			<div class="form-group">
-            				<label class="col-sm-3 control-label text-right">C5. Usia:</label>
-            				<div class="col-sm-8">
-            					<select name='bobot_usia' class="required select">
-            						<option value="1">Sangat Rendah</option>
-            						<option value="2">Rrendah</option>
-            						<option value="3">Cukup</option>
-            						<option value="4">Tinggi</option>
-            						<option value="5">Sangat Tinggi</option>
-            					</select>
-            				</div>
-            			</div>
 
             			<div class="form-action text-right">
             				<input type="submit" name="simpan" value="Proses" class="btn btn-primary">
--- aqua/admin/analisa_hasil.php
+++ aqua/admin/analisa_hasil.php
@@ -1,159 +1,158 @@
 <?php include "head.php"; ?>
 <!-- Body -->
 
-			<!-- Link Menu -->
-			<?php include "menu.php"; ?>
+<!-- Link Menu -->
+<?php include "menu.php"; ?>
+</div>
+<br />
 
-			</div>
-		<br />
+<div id="content">
+	<!-- Page title -->
+	<div class="page-title">
+		<h5><i class="fa fa-desktop"></i> Hasil Analisa</h5>
+	</div>
+	<!-- /page title -->
 
-		<div id="content">
-		<!-- Page title -->
-			<div class="page-title">
-				<h5><i class="fa fa-desktop"></i> Hasil Analisa</h5>
-			</div>
-			<!-- /page title -->
+	<!-- Hover rows datatable inside panel -->
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h6 class="panel-title">
+				<tr align="right">
+					<th></th>
+					<th>Bobot :</th>
+					<th><?php echo "(" . $_POST['bobot_nilai_akhir'] . ")"; ?></th>
+					<th><?php echo "(" . $_POST['bobot_sertifikat'] . ")"; ?></th>
+					<th><?php echo "(" . $_POST['bobot_nilai_sikap'] . ")"; ?></th>
+					<!-- <th><?php echo "(" . $_POST['bobot_tanggungan'] . ")"; ?></th> -->
+					<!-- <th><?php echo "(" . $_POST['bobot_usia'] . ")"; ?></th> -->
+				</tr>
+			</h6>
+		</div>
+		<div class="datatable">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>C1. Nilai AKHIR (Benefit)</th>
+						<th>C2. Jumlah Sertifikat (Benefit)</th>
+						<th>C3. Nilai Sikap (Benefit)</th>
+						<!-- <th>C4. Jumlah Tanggungan (cost)</th> -->
+						<!-- <th>C5. Usia (Benefit)</th>            					 -->
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$nomor = 0;
+					$hasil = mysqli_query($conn, "select * from klasifikasi, calonbeasiswa where klasifikasi.id_mhs=calonbeasiswa.id_mhs");
+					while ($dataku = mysqli_fetch_array($hasil)) {
+					?>
+						<tr>
+							<td><?php echo $nomor = $nomor + 1; ?></td>
+							<td><?php echo $dataku['nama_mhs']; ?></td>
+							<td><?php echo $dataku['nilai_akhir']; ?></td>
+							<td><?php echo $dataku['sertifikat']; ?></td>
+							<td><?php echo $dataku['nilai_sikap']; ?></td>
+							<!-- <td><?php echo $dataku['jml_tanggungan']; ?></td> -->
+							<!-- <td><?php echo $dataku['usia']; ?></td> -->
+						</tr>
+					<?php }	?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 
-			<!-- Hover rows datatable inside panel -->
-            <div class="panel panel-default">
-            	<div class="panel-heading"><h6 class="panel-title">
-            				<tr align="right">
-            					<th></th>
-            					<th>Bobot :</th>
-            					<th><?php echo "(" .$_POST['bobot_nilai_ipk']. ")"; ?></th>
-            					<th><?php echo "(" .$_POST['bobot_penghasilan_ortu']. ")"; ?></th>
-            					<th><?php echo "(" .$_POST['bobot_semester']. ")"; ?></th>
-            					<th><?php echo "(" .$_POST['bobot_tanggungan']. ")"; ?></th>
-            					<th><?php echo "(" .$_POST['bobot_usia']. ")"; ?></th>
-            				</tr>
-            	</h6></div>
-            	<div class="datatable">
-            		<table class="table table-hover">
-            			<thead>
-            				<tr>
-            					<th>No</th>
-            					<th>Nama</th>
-            					<th>C1. Nilai IPK (Benefit)</th>
-            					<th>C2. Penghasilan Ortu (Cost)</th>
-            					<th>C3. Semester (Benefit)</th>
-            					<th>C4. Jumlah Tanggungan (cost)</th>
-            					<th>C5. Usia (Benefit)</th>            					
-            				</tr>
-            			</thead>
-            			<tbody>
-            				<?php
-            				$nomor = 0;
-            				$hasil = mysqli_query($conn,"select * from klasifikasi, calonbeasiswa where klasifikasi.id_mhs=calonbeasiswa.id_mhs");
-            				while ($dataku = mysqli_fetch_array($hasil)) {
-            				?>
-            				<tr>
-            					<td><?php echo $nomor=$nomor+1; ?></td>
-            					<td><?php echo $dataku['nama_mhs']; ?></td>
-            					<td><?php echo $dataku['nilai_ipk']; ?></td>
-            					<td><?php echo $dataku['penghasilan_ortu']; ?></td>
-            					<td><?php echo $dataku['semester']; ?></td>
-            					<td><?php echo $dataku['jml_tanggungan']; ?></td>
-            					<td><?php echo $dataku['usia']; ?></td>
-            				</tr>
-            				<?php }	?>            				
-            			</tbody>
-            		</table>
-            	</div>
-            </div>                     				
-            			
-            <!-- /hover rows datatable inside panel -->
-<!-- Cari nilai maximal dan minimal-->
-<?php
-#Cari nilai maximal
-$carimax = mysqli_query($conn,"SELECT max(nilai_ipk) as max1,
-								max(penghasilan_ortu) as max2,
-								max(semester) as max3,
-								max(jml_tanggungan) as max4,
-								max(usia) as max5
-								FROM klasifikasi");
-$max = mysqli_fetch_array($carimax);
-# Cari nilai minimal
-$carimin = mysqli_query($conn,"SELECT min(nilai_ipk) as min1,
-								min(penghasilan_ortu) as min2,
-								min(semester) as min3,
-								min(jml_tanggungan) as min4,
-								min(usia) as min5
-								FROM klasifikasi");
-$min = mysqli_fetch_array($carimin);
-?>
-				<div class="panel panel-default">
-				<div class="panel-heading"><h6 class="panel-titl~e">Normalisasi</h6></div>
-				<div class="datatable">
-					<table class="table table-hover">
-						<thead>
-							<tr>
-								<th>No</th>
-								<th>Nama</th>
-								<th>C1. Nilai IPK</th>
-								<th>C2. Penghasilan Ortu</th>
-								<th>C3. Semester</th>
-								<th>C4. Jumlah Tanggungan</th>
-								<th>C5. Usia</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php
-							$nomor=0;
-							$hasil = mysqli_query($conn,"select * from klasifikasi, calonbeasiswa where klasifikasi.id_mhs=calonbeasiswa.id_mhs");
-							while ($dataku = mysqli_fetch_array($hasil)) {
-							?>
-							<tr>
-								<td><?php echo $nomor=$nomor+1; ?></td>
-								<td><?php echo $dataku['nama_mhs']; ?></td>
-								<td><?php echo round($dataku['nilai_ipk']/$max['max1'],2);?></td>
-								<td><?php echo round($min['min2']/$dataku['penghasilan_ortu'],2); ?></td>
-								<td><?php echo round($dataku['semester']/$max['max3'],2); ?></td>
-								<td><?php echo round($min['min4']/$dataku['jml_tanggungan'],2); ?></td>
-								<td><?php echo round($dataku['usia']/$max['max5'],2); ?></td>
-							</tr>
-							<?php } ?>
-						</tbody>
-					</table>
-				</div>
-				</div>
-				<!-- /hover rows datatable inside panel -->
-<?php
-$bobot_nilai_ipk		= $_POST['bobot_nilai_ipk'];
-$bobot_penghasilan_ortu	= $_POST['bobot_penghasilan_ortu'];
-$bobot_semester			= $_POST['bobot_semester'];
-$bobot_tanggungan		= $_POST['bobot_tanggungan'];
-$bobot_usia				= $_POST['bobot_usia'];
-?>
-				<div class="panel panel-default">
-				<div class="panel-heading"><h6 class="panel-title">Perangkingan</h6></div>
-				<div class="datatable">
-					<table class="table table-hover">
-						<thead>
-							<tr>
-								<th>No</th>
-								<th>Nama</th>
-								<th>Nilai</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php
-							$nomor=0;
-							$hasil = mysqli_query($conn,"select * from klasifikasi, calonbeasiswa where klasifikasi.id_mhs=calonbeasiswa.id_mhs");
-							while ($dataku = mysqli_fetch_array($hasil)) {
-							?>
-							<tr>
-								<td><?php echo $nomor=$nomor+1; ?></td>
-								<td><?php echo $dataku['nama_mhs']; ?></td>
-								<td><?php echo round((($dataku['nilai_ipk']/$max['max1'])*$bobot_nilai_ipk)+
-								(($min['min2']/$dataku['penghasilan_ortu'])*$bobot_penghasilan_ortu)+
-								(($dataku['semester']/$max['max3'])*$bobot_semester)+
-								(($min['min4']/$dataku['jml_tanggungan'])*$bobot_tanggungan)+
-								(($dataku['usia']/$max['max5'])*$bobot_usia),2); ?></td>
-							</tr>
-							<?php }	?>
-						</tbody>
-					</table>
-				</div>
-				</div>
-				<!-- /hover rows datatable inside panel -->
-<?php include "footer.php"; ?>
+	<!-- /hover rows datatable inside panel -->
+	<!-- Cari nilai maximal dan minimal-->
+	<?php
+	#Cari nilai maximal
+	// $carimax = mysqli_query($conn, "SELECT max(nilai_akhir) as max1,
+	// 							max(sertifikat) as max2,
+	// 							max(nilai_sikap) as max3,
+	// 							FROM klasifikasi");
+
+	$carimax = mysqli_query($conn, "SELECT MAX(nilai_akhir) as max1, MAX(sertifikat) as max2, MAX(nilai_sikap) as max3 FROM `klasifikasi`");
+	$max = mysqli_fetch_array($carimax);
+
+	
+	# Cari nilai minimal
+	$carimin = mysqli_query($conn, "SELECT MIN(nilai_akhir) as min1, MIN(sertifikat) as min2, MIN(nilai_sikap) as min3 FROM `klasifikasi`");
+	$min = mysqli_fetch_array($carimin);
+	
+	?>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h6 class="panel-titl~e">Normalisasi</h6>
+		</div>
+		<div class="datatable">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>C1. Nilai AKHIR (Benefit)</th>
+						<th>C2. Jumlah Sertifikat (Benefit)</th>
+						<th>C3. Nilai Sikap (Benefit)</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$nomor = 0;
+					$hasil = mysqli_query($conn, "select * from klasifikasi, calonbeasiswa where klasifikasi.id_mhs=calonbeasiswa.id_mhs");
+					while ($dataku = mysqli_fetch_array($hasil)) {
+					?>
+						<tr>
+							<td><?php echo $nomor = $nomor + 1; ?></td>
+							<td><?php echo $dataku['nama_mhs']; ?></td>
+							<td><?php echo round($dataku['nilai_akhir'] / $max['max1'], 2); ?></td>
+							<td><?php echo round($min['min2'] / $dataku['sertifikat'], 2); ?></td>
+							<td><?php echo round($dataku['nilai_sikap'] / $max['max3'], 2); ?></td>
+							<!-- <td><?php echo round($min['min4'] / $dataku['jml_tanggungan'], 2); ?></td> -->
+							<!-- <td><?php echo round($dataku['usia'] / $max['max5'], 2); ?></td> -->
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<!-- /hover rows datatable inside panel -->
+	<?php
+	$bobot_nilai_ipk		= $_POST['bobot_nilai_akhir'];
+	$bobot_penghasilan_ortu	= $_POST['bobot_sertifikat'];
+	$bobot_semester			= $_POST['bobot_nilai_sikap'];
+	// $bobot_tanggungan		= $_POST['bobot_tanggungan'];
+	// $bobot_usia				= $_POST['bobot_usia'];
+	?>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h6 class="panel-title">Perangkingan</h6>
+		</div>
+		<div class="datatable">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>Nilai</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$nomor = 0;
+					$hasil = mysqli_query($conn, "select * from klasifikasi, calonbeasiswa where klasifikasi.id_mhs=calonbeasiswa.id_mhs");
+					while ($dataku = mysqli_fetch_array($hasil)) {
+					?>
+						<tr>
+							<td><?php echo $nomor = $nomor + 1; ?></td>
+							<td><?php echo $dataku['nama_mhs']; ?></td>
+							<td><?php echo round((($dataku['nilai_akhir'] / $max['max1']) * $bobot_nilai_ipk) +
+									(($min['min2'] / $dataku['sertifikat']) * $bobot_penghasilan_ortu) +
+									(($dataku['nilai_sikap'] / $max['max3']) * $bobot_semester), 2); ?></td>
+						</tr>
+					<?php }	?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<!-- /hover rows datatable inside panel -->
+	<?php include "footer.php"; ?>
